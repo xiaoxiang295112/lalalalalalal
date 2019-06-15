ef249e5415823885ae741b8fe660a82cb7c13d3f
xiaoxiang <420106233@qq.com>
2019-06-15 19:29:42 +0800

    创建get11工程

---
+++ git11/src/main/java/com/itheima/Test.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("喜喜喜喜喜喜");
+        System.out.println("喜喜喜喜喜喜");
+        System.out.println("喜喜喜喜喜喜");
+        System.out.println("喜喜喜喜喜喜");
+        System.out.println("喜喜喜喜喜喜");
+        System.out.println("喜喜喜喜喜喜");
+
+    }
+}
